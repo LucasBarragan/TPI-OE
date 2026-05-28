1bbf9118fbddcf4015b59d142022263d638512da
LucasBarragan <lucasbarragan000@gmail.com>
2026-05-28 16:57:55 -0300

    Creación del menú de prueba principal del chatbot

---
+++ TPI-OE.py
@@ -0,0 +1,19 @@
+print("=== SISTEMA DE GESTIÓN DE VACACIONES ===")
+
+print("1. Solicitar vacaciones")
+print("2. Consultar saldo de días")
+print("3. Salir")
+
+opcion = input("Seleccione una opción: ")
+
+if opcion == "1":
+    print("Has seleccionado solicitar vacaciones")
+
+elif opcion == "2":
+    print("Consulta de saldo de días")
+
+elif opcion == "3":
+    print("Saliendo del sistema")
+
+else:
+    print("Opción inválida")
