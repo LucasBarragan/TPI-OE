7de8d3760683ee73e2894d764e42ec7eac1e2e8d
LucasBarragan <lucasbarragan000@gmail.com>
2026-06-04 15:24:55 -0300

    Implementacion de la opcion 2 (registrar proveedores) / validacion completa de datos de entrada para opcion 2

--- TPI-OE.py
+++ TPI-OE.py
@@ -1,34 +1,132 @@
 import csv
 
-def consultar_proveedor():
+# carga de proveedores
+
+def cargar_proveedores():
+    try:
+        with open("proveedores.csv", "r", encoding="utf-8") as archivo:
+            lector = csv.DictReader(archivo)
+            return list(lector)
+
+    except FileNotFoundError:
+        print("Error: No se encontró el archivo de proveedores.")
+        return []
+
+# Consultar proveedor (1)
+
+def consultar_proveedor(proveedores):
     cuit_buscado = input("Ingrese el CUIT: ").strip()
+
     if cuit_buscado == "":
         print("Error: Debe ingresar un CUIT.")
         return
 
-    try:
-        with open("proveedores.csv", "r", encoding="utf-8") as archivo:
-            lector = csv.DictReader(archivo)
+    for proveedor in proveedores:
+        if proveedor["cuit"] == cuit_buscado:
+            print("\n=== PROVEEDOR ENCONTRADO ===")
+            print("CUIT:", proveedor["cuit"])
+            print("Razón social:", proveedor["razon_social"])
+            print("Rubro:", proveedor["rubro"])
+            print("Email:", proveedor["email"])
+            print("Teléfono:", proveedor["telefono"])
+            return
+    print("Proveedor no registrado.")
 
-            encontrado = False
 
-            for proveedor in lector:
-                if proveedor["cuit"] == cuit_buscado:
-                    encontrado = True
+def registrar_proveedor(proveedores):
 
-                    print("\n=== PROVEEDOR ENCONTRADO ===")
-                    print("CUIT:", proveedor["cuit"])
-                    print("Razón social:", proveedor["razon_social"])
-                    print("Rubro:", proveedor["rubro"])
-                    print("Email:", proveedor["email"])
-                    print("Teléfono:", proveedor["telefono"])
-                    break
+    # VALIDACIÓN DE CUIT
+    while True:
+        cuit = input("Ingrese el CUIT (11 números): ").strip()
 
-            if not encontrado:
-                print("Proveedor no registrado.")
+        if cuit == "":
+            print("Error: Debe ingresar un CUIT.")
+            continue
 
-    except FileNotFoundError:
-        print("Error: No se encontró el archivo de proveedores.")
+        if not cuit.isdigit():
+            print("Error: El CUIT debe contener solo números.")
+            continue
+
+        if len(cuit) != 11:
+            print("Error: El CUIT debe tener exactamente 11 números.")
+            continue
+
+        break
+
+    # VERIFICAR SI YA EXISTE
+    for proveedor in proveedores:
+        if proveedor["cuit"] == cuit:
+            print("El proveedor ya se encuentra registrado.")
+            return
+
+    # RAZÓN SOCIAL
+    while True:
+        razon_social = input("Ingrese la razón social: ").strip()
+
+        if razon_social == "":
+            print("Error: Debe ingresar una razón social.")
+        else:
+            break
+
+    # RUBRO
+    while True:
+        rubro = input("Ingrese el rubro: ").strip()
+
+        if rubro == "":
+            print("Error: Debe ingresar un rubro.")
+        else:
+            break
+
+    # EMAIL
+    while True:
+        email = input("Ingrese el email: ").strip()
+
+        if email == "":
+            print("Error: Debe ingresar un email.")
+            continue
+
+        if "@" not in email:
+            print("Error: Email inválido.")
+            continue
+
+        break
+
+    # TELÉFONO
+    while True:
+        telefono = input("Ingrese el teléfono: ").strip()
+
+        if telefono == "":
+            print("Error: Debe ingresar un teléfono.")
+            continue
+
+        if not telefono.isdigit():
+            print("Error: El teléfono debe contener solo números.")
+            continue
+
+        if len(telefono) < 8:
+            print("Error: El teléfono debe tener al menos 8 dígitos.")
+            continue
+
+        if len(telefono) > 15:
+            print("Error: El teléfono no puede superar los 15 dígitos.")
+            continue
+
+        break
+
+    nuevo_proveedor = {
+        "cuit": cuit,
+        "razon_social": razon_social,
+        "rubro": rubro,
+        "email": email,
+        "telefono": telefono
+    }
+
+    proveedores.append(nuevo_proveedor)
+
+
+    print("Proveedor registrado correctamente.")
+proveedores = cargar_proveedores()
+
 
 while True:
     print("\n=== SISTEMA DE PROVEEDORES ===")
@@ -37,13 +135,13 @@ while True:
     print("3. Mostrar proveedores")
     print("4. Salir")
 
-    opcion = input("Seleccione una opción: ")
+    opcion = input("Seleccione una opción: ").strip()
 
     if opcion == "1":
-        consultar_proveedor()
+        consultar_proveedor(proveedores)
 
     elif opcion == "2":
-        print("Registrar proveedor")
+        registrar_proveedor(proveedores)
 
     elif opcion == "3":
         print("Mostrar proveedores")
